978dede03bb4eded3aee4f0ab2a3209d09ad3ad3
Bernard Niset <bernard.niset@gmail.com>
2019-02-17 17:25:10 +0100

    Minor improvement to build.rs

--- build.rs
+++ build.rs
@@ -1,16 +1,16 @@
-extern crate chrono;
 
 use chrono::Local;
 use std::process::Command;
 
+const UNKNOWN: &str = "unknown";
+
 fn main() {
-    let unknown: Result<String, &str> = Ok("unknown".to_string());
     let output = Command::new("git")
         .args(&["rev-parse", "--short", "HEAD"])
         .output();
     let git_hash = match output {
         Ok(output) => String::from_utf8(output.stdout).unwrap(),
-        Err(_) => std::env::var("TRAVIS_COMMIT").or(unknown).unwrap(),
+        Err(_) => std::env::var("TRAVIS_COMMIT").unwrap_or(UNKNOWN.into()),
     };
 
     println!("cargo:rustc-env=GIT_COMMIT={}", git_hash);
