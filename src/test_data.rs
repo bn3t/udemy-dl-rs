e645d57055aafa3e33eb4964ab3d461784ae584c
Bernard Niset <bernard.niset@gmail.com>
2019-03-02 20:55:25 +0100

    Apply cargo fmt fix

--- src/test_data.rs
+++ src/test_data.rs
@@ -1,25 +1,25 @@
 use crate::model::*;
 
 pub fn make_course() -> Course {
-  Course {
-    id: 54321,
-    url: "the-url".into(),
-    published_title: "css-the-complete-guide-incl-flexbox-grid-sass".into(),
-  }
+    Course {
+        id: 54321,
+        url: "the-url".into(),
+        published_title: "css-the-complete-guide-incl-flexbox-grid-sass".into(),
+    }
 }
 
 pub fn make_test_course_content() -> CourseContent {
-  CourseContent {
-    chapters: vec![Chapter {
-      object_index: 1,
-      title: "The Chapter".into(),
-      lectures: vec![Lecture {
-        has_video: true,
-        filename: "blah-blah.mp4".into(),
-        id: 4321,
-        object_index: 1,
-        title: "The Lecture".into(),
-      }],
-    }],
-  }
+    CourseContent {
+        chapters: vec![Chapter {
+            object_index: 1,
+            title: "The Chapter".into(),
+            lectures: vec![Lecture {
+                has_video: true,
+                filename: "blah-blah.mp4".into(),
+                id: 4321,
+                object_index: 1,
+                title: "The Lecture".into(),
+            }],
+        }],
+    }
 }
